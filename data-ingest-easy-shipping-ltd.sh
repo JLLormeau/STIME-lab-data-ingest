3c2c514298b0c20d6a611421eec94bcbf2ccccf5
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-06-15 14:23:43 +0200

    Update data-ingest-easy-shipping-ltd.sh

--- data-ingest-easy-shipping-ltd.sh
+++ data-ingest-easy-shipping-ltd.sh
@@ -9,16 +9,16 @@ while true
 
         date
         value=$(( ( RANDOM % 10 )  + 1 ))
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.fuel.total,trucknr=99,model=mac-granite $((100*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.fuel.usage,trucknr=99,model=mac-granite $((2*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.operation.hours,trucknr=99,model=mac-granite $((100*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.fuel.total,trucknr=12,model=mac-anthem $((4*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.fuel.usage,trucknr=12,model=mac-anthem $((8*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""truck.operation.hours,trucknr=12,model=mac-anthem $((5*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""container.temperature,containernr=234321,trucknr=99 $((4*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""container.temperature.dev,containernr=234321,trucknr=99 $((0,1*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""container.temperature,containernr=111111,trucknr=12 $((4*$value))" "$URL_DT"
-        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$prefix""container.temperature.dev,containernr=111111,trucknr=12 $((1,5*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.fuel.total,trucknr=99,model=mac-granite $((100*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.fuel.usage,trucknr=99,model=mac-granite $((2*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.operation.hours,trucknr=99,model=mac-granite $((100*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.fuel.total,trucknr=12,model=mac-anthem $((4*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.fuel.usage,trucknr=12,model=mac-anthem $((8*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""truck.operation.hours,trucknr=12,model=mac-anthem $((5*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""container.temperature,containernr=234321,trucknr=99 $((4*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""container.temperature.dev,containernr=234321,trucknr=99 $((0,1*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""container.temperature,containernr=111111,trucknr=12 $((4*$value))" "$URL_DT"
+        curl -H "Authorization: Api-Token "$MyToken"" -X POST  -H "$Header" --data-ascii "$Prefix""container.temperature.dev,containernr=111111,trucknr=12 $((1,5*$value))" "$URL_DT"
 
         sleep 60
   done
